c0e85a268402c3f29684e6e7263b24837446a2f0
rakesh <rak1994kumar@gmail.com>
2017-07-05 20:46:52 +0530

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,8 @@ var fs = require('fs'),
     imagemin = require('gulp-imagemin'),
     useref = require('gulp-useref'),
     clean = require('gulp-clean'),
-    // jshint = require('gulp-jshint'),
-    // stylish = require('jshint-stylish'),
+    //// jshint = require('gulp-jshint'),
+    //// stylish = require('jshint-stylish'),
     autoprefixer = require('gulp-autoprefixer'),
     minifyCss = require('gulp-clean-css'),
     browser_Sync = require('browser-sync').create(); // create a browser sync instance.
@@ -48,13 +48,13 @@ if (!fs.existsSync(sourceDir)) {
 
 gulp.task('styles', function () {
 
-    var scssStream = gulp.src(inputActPath + 'scss/**/*.scss')
+    var scssStream = gulp.src('public_development/scss/**/*.scss')
         .pipe(sass())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
         .pipe(concat('scss-files.scss'))
         ;
 
-    var cssStream = gulp.src(inputActPath + 'css/**/*.css')
+    var cssStream = gulp.src('public_development/css/**/*.css')
         .pipe(concat('css-files.css'))
         .pipe(gulpif('*.css', minifyCss()))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
@@ -63,22 +63,22 @@ gulp.task('styles', function () {
         .pipe(concat('styles.min.css'))
         .pipe(cssnano())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(gulp.dest(outputActPath + 'css'));
+        .pipe(gulp.dest('public/css'));
 
     return mergedStream;
 });
 
 gulp.task('images', function () {
-    return gulp.src(inputActPath + 'images/**/*.+(png|jpg|jpeg|gif|svg)')
+    return gulp.src('public_development/images/**/*.+(png|jpg|jpeg|gif|svg)')
         .pipe(cache(imagemin({
             interlaced: true
         })))
-        .pipe(gulp.dest(outputActPath +'images'));
+        .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('js', function () {
 
-    gulp.src(inputActPath + 'js/*.js')
+    gulp.src('public_development/js/*.js')
         .pipe(minify({
             ext: {
                 src: '.min.js',
@@ -88,22 +88,21 @@ gulp.task('js', function () {
             ignoreFiles: []
         }))
         .pipe(gulpif('*.js', uglify()))
-
-        .pipe(gulp.dest(outputActPath + 'js/'));
-    console.log('Files and folders that would be deleted:\n');
+        .pipe(gulp.dest('public/js/'));
+    console.log('Files and folders that would be deleted ');
 });
 
 
 gulp.task('views', function buildHTML() {
-    return gulp.src(inputActPath + '*.html')
+    return gulp.src('public_development/**/*.html')
         .pipe(useref())
-        .pipe(gulp.dest(outputActPath));
+        .pipe(gulp.dest('public/'));
 });
 
 gulp.task('browser-sync', function () {
     browser_Sync.init({
         server: {
-            baseDir: outputActPath
+            baseDir: 'public/'
         },
        /* proxy: {
         target: "localhost:8080", // can be [virtual host, sub-directory, localhost with port]
@@ -115,19 +114,19 @@ gulp.task('browser-sync', function () {
 
 
 gulp.task('watch', ['browser-sync', 'styles', 'ng', 'js'], function () {
-    gulp.watch(inputActPath + 'scss/**/*.scss', ['styles']);
-    gulp.watch(inputActPath + 'css/**/*.css', ['styles']);
-    gulp.watch(inputActPath + 'ng/**/*', ['ng']);
-    gulp.watch(inputActPath + '/**/*.html', ['views']);
+    gulp.watch('public_development/scss/**/*.scss', ['styles']);
+    gulp.watch('public_development/css/**/*.css', ['styles']);
+    gulp.watch('public_development/ng/**/*', ['ng']);
+    gulp.watch('public_development/**/*.html', ['views']);
     gulp.watch('public_development/js/lib/*.js', ['jquery-concat']);
-    gulp.watch(inputActPath + '**/*', browser_Sync.reload);
+    gulp.watch('public_development/**/*', browser_Sync.reload);
 
 });
 
 gulp.task('ng', function () {
     console.log("ng is updating");
-    return gulp.src(inputActPath + 'ng/**/*')
-        .pipe(gulp.dest(outputActPath + 'ng'));
+    return gulp.src('public_development/ng/**/*')
+        .pipe(gulp.dest('public/ng'));
 });
 
 /*
@@ -149,7 +148,7 @@ gulp.task('jquery-concat', function() {
 
 
 gulp.task('clean:dist', function () {
-    return del.sync(outputActPath);
+    return del.sync('public/');
 });
 
 gulp.task('build', function (callback) {
@@ -161,9 +160,9 @@ gulp.task('build', function (callback) {
 });
 
 gulp.task('clean-public', function () {
-    return gulp.src(inputActPath)
+    return gulp.src('public_development/')
         .pipe(clean({force: true}))
-        .pipe(gulp.dest(outputActPath));
+        .pipe(gulp.dest('public/'));
 });
 
 gulp.task('default', function (callback) {
