c86bdcc6609cd3a7f27dbb5b1f8692917fe40406
rakesh <rak1994kumar@gmail.com>
2017-07-10 17:51:48 +0530

    karma added

--- public_development/js/timeCal.service.js
+++ public_development/js/timeCal.service.js
@@ -1,23 +1,53 @@
 /**
- *  Time calculator Services
- *  
+ * @namespace BTApp
  */
-
-(function () {
-    var app = angular.module("BTApp");
-    app.service("timeCalculator", function ($scope) {
-
-        var convertDateTimeObjToNumber = function () {
-            console.log("Convert Date Time object to Number ");
-
-        };
+var app = angular.module("BTApp");
 
 
-        var timeCal = {
-            convertDateTimeObjToNumber: convertDateTimeObjToNumber
-        };
+var objeTime = {
+    time: "12:30:00",
+    day: 2
+};
+app.service("timeCalculatorService", function () {
+    /**
+    * @function convertDateTimeObjToNumber
+    * @param  {object} dateTimeObj user to take dateTimeObj as object 
+    * @param  {string} target user to take target as string 
+    * @returns {String} Result is calculation of startTime With day
+    * @author santosh
+    * @description The parameters of a function call are the function's arguments. Arguments are passed to functions by value. If the function changes the value of an argument, this change is not reflected globally or in the calling function. However, object references are values, too, and they are special: if the function changes the referred object's properties, that change is visible outside the function, as shown in the following example:
+    * @since  2017
+    * @version 0.0.1
+    */
+    var convertDateTimeObjToNumber = function (dateTimeObj, target) {
+        if (dateTimeObj === null || dateTimeObj === undefined || typeof dateTimeObj === 'object') {
+            throw new Error("Not valid dateTimeObject passed to method()");
+        }
+        if (dateTimeObj.day === null || dateTimeObj.time === null) {
+            throw new Error("Not valid dateTimeObject passed to convertDateTimeObj()");
+        }
 
-        return timeCal;
-    });
+         if(target===null){
+             target = "";
+         }
+         target = target.toLowerCase();
 
-})();
+         var timeParts = dateTimeObj.time.split(":");
+         var hrs = parseInt(timeParts[0]);
+         var min = parseInt(timeParts[1]);
+
+            if((hrs>23 || hrs<0)&&(min>59 || min<0)){
+                throw new Error("Not valid dateTimeObj.time passed to convertDateTimeObj()");
+            }
+        var mins = (dateTimeObj.day * 1440) + (hrs * 60) + (min);
+
+    };
+
+
+    var timeCal = {
+        convertDateTimeObjToNumber: convertDateTimeObjToNumber
+    };
+
+    return timeCal;
+});
+
