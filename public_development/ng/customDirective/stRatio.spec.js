9e950d0cf7fa8468e09a48f7adea343700cf3d65
rakesh <rak1994kumar@gmail.com>
2017-07-12 20:28:06 +0530

    dsdada

--- public_development/ng/customDirective/stRatio.spec.js
+++ public_development/ng/customDirective/stRatio.spec.js
@@ -1,15 +1,19 @@
 describe("directive:stRation", function () {
 
-    var scope;element,
+    var scope,element;
         beforeEach(module('BTApp'));
 
-        beforeEach(inject(function($rootScope, $compile) {
+    beforeEach(inject(function ($rootScope, $compile) {
 
-element = angular.element('<st-ratio></st-ratio>');
+        element = angular.element('<st-ratio></st-ratio>');
+
+        scope = $rootScope;
+        $compile(elm)(scope);
+        scope.$digest();
 
 
-        }));
+    }));
 
 
-    });
+});
 
